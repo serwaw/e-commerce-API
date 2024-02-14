8fffce5c9cdeb0b907ff094e752bfc0bdbd1b769
Serwa Waisi <serwa.waisi@skatteetaten.no>
2024-02-14 21:29:57 +0100

    Add the main class

---
+++ main/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@Import({
+    ApiConfiguration.class,
+    ProductService.class
+})
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
